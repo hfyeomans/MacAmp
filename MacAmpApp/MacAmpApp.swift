73cac84d7a7b7635685b6ec5d8ed434c21ef24a5
Hank Yeomans <hank.yeomans@me.com>
2025-10-24 20:23:38 -0400

    Simplify menus and replace File>New with Open

--- MacAmpApp/MacAmpApp.swift
+++ MacAmpApp/MacAmpApp.swift
@@ -28,7 +28,7 @@ struct MacAmpApp: App {
 
         // Commands are defined once here and apply to all window groups
         .commands {
-            AppCommands(dockingController: dockingController, skinManager: skinManager)
+            AppCommands(dockingController: dockingController, audioPlayer: audioPlayer)
             SkinsCommands(skinManager: skinManager)
         }
     }
